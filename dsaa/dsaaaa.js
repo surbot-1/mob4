602f4b9eacf2aa1a1f20fb92e1710487e8eec585
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-03-31 22:47:17 +0530

    Update dsaaaa.js

--- dsaa/dsaaaa.js
+++ dsaa/dsaaaa.js
@@ -9,7 +9,7 @@
         
         var image1 = new Image();  // dp 144
         image1.setAttribute('crossOrigin', 'anonymous');
-        image1.src = 'image/dp4.png';
+        image1.src = 'image/dp8.png';
         image1.onload = function (e) {
         var x = canx;
 	var y = cany;
@@ -17,16 +17,60 @@
 	var h = 144;
           context.drawImage(image1, x, y, w, h);
         }; 
-	
-        var image2 = new Image();  // text 144
+	 
+	var image2 = new Image();  // dp 144
         image2.setAttribute('crossOrigin', 'anonymous');
-        image2.src = 'image/text4.png';
+        image2.src = 'image/dp9.png';
         image2.onload = function (e) {
+        var x = canx;
+	var y = cany+144;
+	var w = canw;
+	var h = 144;
+          context.drawImage(image2, x, y, w, h);
+        }; 
+	 
+	var image3 = new Image();  // dp 144
+        image3.setAttribute('crossOrigin', 'anonymous');
+        image3.src = 'image/dp9.png';
+        image3.onload = function (e) {
+        var x = canx;
+	var y = cany+144*2;
+	var w = canw;
+	var h = 144;
+          context.drawImage(image3, x, y, w, h);
+        }; 
+	 
+	var image4 = new Image();  // dp 144
+        image4.setAttribute('crossOrigin', 'anonymous');
+        image4.src = 'image/dp9.png';
+        image4.onload = function (e) {
+        var x = canx;
+	var y = cany+144*3;
+	var w = canw;
+	var h = 144;
+          context.drawImage(image4, x, y, w, h);
+        }; 
+	 
+	var image5 = new Image();  // dp 144
+        image5.setAttribute('crossOrigin', 'anonymous');
+        image5.src = 'image/dp9.png';
+        image5.onload = function (e) {
+        var x = canx;
+	var y = cany+144*4;
+	var w = canw;
+	var h = 144;
+          context.drawImage(image5, x, y, w, h);
+        }; 
+	
+        var image10 = new Image();  // text 144
+        image10.setAttribute('crossOrigin', 'anonymous');
+        image10.src = 'image/dp10.png';
+        image10.onload = function (e) {
 	var x = canx;
 	var y = canh-144-640+640;  // 1391+640=2032
 	var w = canw;
 	var h = 144;
-          context.drawImage(image2, x, y, w, h);
+          context.drawImage(image10, x, y, w, h);
         }; 
 	  
 	var timer;
