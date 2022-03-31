5538435ed7a07b26058036d54e4b83a7a36181eb
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-03-31 21:30:38 +0530

    Update qwertypad.js

--- function/qwertypad.js
+++ function/qwertypad.js
@@ -188,7 +188,7 @@ function writeChar() {
        if (ptr==0) {
        } else if(keyP==0x0A) {    // newline 
 	       var i=0; var j=0;
-	       for (let k=0; k<(ptr-2); k++) {
+	       for (let k=0; k<(ptr-1); k++) {
                   if (msgArr[k]==0x0A) {
 		   i=0; j++; 
 		   if (j>1) {i=0; j=0;}
