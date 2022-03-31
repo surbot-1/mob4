bea83d0d78b108d916975a1adc8858ea871afb80
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-01 00:41:17 +0530

    Update ipaddress.js

--- function/ipaddress.js
+++ function/ipaddress.js
@@ -5,20 +5,20 @@
              for(let i = 0; i < 2; i++){
                   ip[i]=data[i];
                  if (i==1 && data[i]!=null) {
-                  iprs=true;
-                  ipRcv= ip[1];
+                  ipsts=true;
+                  iprcv= ip[1];
                  }
                }
            })();
         }  
                
         
-          function detectOnline() {
+         function detectOnline() {
                if (navigator.onLine) {
                    if (!ipReceived){
                         getIP(); 
                     }
                } else {
-                   iprs=false;
+                   ipsts=false;
                }
           }
