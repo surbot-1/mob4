26cad4c574572b6e38c8825655037ec9fa56714e
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-03-31 13:25:23 +0530

    Update dsaaaa.js

--- dsaa/dsaaaa.js
+++ dsaa/dsaaaa.js
@@ -5,11 +5,11 @@
     var context = canvas.getContext('2d');
 	  
 	context.fillStyle = "white";
-        context.fillRect(0, 0, 1080, 2400);
+        context.fillRect(0, 0, 1080, 2176);
         
         var image1 = new Image();  // dp 144
         image1.setAttribute('crossOrigin', 'anonymous');
-        image1.src = 'dp3.png';
+        image1.src = 'image/dp3.png';
         image1.onload = function (e) {
         var x = cx;
 	var y = cy;
@@ -20,7 +20,7 @@
 	
         var image2 = new Image();  // text 144
         image2.setAttribute('crossOrigin', 'anonymous');
-        image2.src = 'text4.png';
+        image2.src = 'image/text4.png';
         image2.onload = function (e) {
 	var x = cx;
 	var y = ch-144-640+640;  // 1391+640=2032
