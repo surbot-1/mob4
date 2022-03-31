61d2d167b803b1aaaaff6206e8970df1f3bffc9b
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-03-31 15:21:27 +0530

    Update cursor.js

--- function/cursor.js
+++ function/cursor.js
@@ -1,1 +1,35 @@
+function writeCursor() {
+        var cvs = document.getElementById("canvas");
+        var ctx = cvs.getContext('2d');
+	
+        ctx.fillStyle = "blue";
+        ctx.fillRect(cx, cy, 4, 48);
+	setTimeout( function() {
+	ctx.fillStyle = "white";
+        ctx.fillRect(cx, cy, 4, 48);
+        }, 0500);
+      }	 
 
+function showCursor() {
+        var cvs = document.getElementById("canvas");
+        var ctx = cvs.getContext('2d');
+        ctx.fillStyle = "blue";
+        ctx.fillRect(cx, cy, 4, 48);
+        }
+	
+function updateCursor() {
+        var cvs = document.getElementById("canvas");
+        var ctx = cvs.getContext('2d');
+        ctx.fillStyle = "white";
+        ctx.fillRect(cxp, cyp, 4, 48);
+	ctx.fillStyle = "blue";
+        ctx.fillRect(cx, cy, 4, 48);
+        }
+	
+function clrCursor() {
+        var cvs = document.getElementById("canvas");
+        var ctx = cvs.getContext('2d');
+        clearInterval(cursor);
+        ctx.fillStyle = "white";
+        ctx.fillRect(cx, cy, 4, 48);
+        }
