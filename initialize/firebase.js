7c82e3fdd0644f72f769bae33d0bec2e080bd8cf
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-01 00:06:54 +0530

    Update firebase.js

--- initialize/firebase.js
+++ initialize/firebase.js
@@ -1,1 +1,13 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCmNVaTr8sTKQVSqZprpBK-AMTpUsNPUGs",
+  authDomain: "surbot-v1-1.firebaseapp.com",
+  databaseURL: "https://surbot-v1-1-default-rtdb.firebaseio.com",
+  projectId: "surbot-v1-1",
+  storageBucket: "surbot-v1-1.appspot.com",
+  messagingSenderId: "702504911981",
+  appId: "1:702504911981:web:6d1fea970a851c7ff3ab64"
+  };
+    
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
