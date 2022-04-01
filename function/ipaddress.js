a73b70371b9f3e41bbb479456a0f08dca24085ba
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-01 10:21:59 +0530

    Update ipaddress.js

--- function/ipaddress.js
+++ function/ipaddress.js
@@ -1,12 +1,13 @@
       function getIP() {
-         var ip=[];
+         var ip=[]; 
          (async function () {
           let  data = await getIPs();
              for(let i = 0; i < 2; i++){
                   ip[i]=data[i];
                  if (i==1 && data[i]!=null) {
                   ipsts=true;
-                  ipads= ip[1]; 
+                  ipads=ip[1]; 
+                  return ip[1]; 
                  }
                }
            })();
