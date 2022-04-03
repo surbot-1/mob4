04b4a5e795c592fed244cdbf39d41265774321f3
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-04 01:50:38 +0530

    Update imagedata.js

--- function/imagedata.js
+++ function/imagedata.js
@@ -1,14 +1,14 @@
 function saveImageData(x, y, w, h) { 
 	var cnv = document.getElementById("canvas"); 
 	var ctx = cnv.getContext('2d'); 
-	var imgData = ctx.createImageData(w, h); 
-	imgData = ctx.getImageData(x, y, w, h); 
-	for (let i=0; i<w*4*h; i+=4) { 
+        // var imgData = ctx.createImageData(w, h); 
+        var imgData = ctx.getImageData(x, y, w, h); 
+        /* for (let i=0; i<w*4*h; i+=4) { 
 		imageView[i+0]=imgData.data[i+0]; 
 		imageView[i+1]=imgData.data[i+1]; 
 		imageView[i+2]=imgData.data[i+2]; 
 		imageView[i+3]=imgData.data[i+3]; 
-	}  aler((imgData.data[0])); aler((imageView[0])); 
+	} */ aler((imgData.data[0])); aler((imageView[0])); 
 	ctx.putImageData(imgData, 500, 500);
 } 
 
