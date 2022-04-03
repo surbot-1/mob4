1b63339c032212f8692122d0faa943e44c5bd896
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-04 02:47:17 +0530

    Update saveandresimage.js

--- function/saveandresimage.js
+++ function/saveandresimage.js
@@ -1,7 +1,7 @@
-function saveImage(x, y, w, h) { 
+function saveImage(x, y, w, h) { alert('1');
   var can = document.getElementById("canvas"); 
   var ctx = can.getContext("2D"); 
-  var imgData = ctx.createImageData(w, h); 
+  var imgData = ctx.createImageData(w, h); alert('2');
   imgData = ctx.getImageData(20, 200, 100, 100); 
   ctx.putImageData(imgData, 20, 400); 
   }
