e1dd9cd8f6000cc4172a23294b919d457386b639
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-03 18:56:50 +0530

    Update fileViewerUrl.js

--- fileViewer/fileViewerUrl.js
+++ fileViewer/fileViewerUrl.js
@@ -1,4 +1,4 @@
-function fileViewerUrl(url) {  
+function fileViewerUrl(x, y, url) {  
   
   var index = url.indexOf('.'); 
   var filename = url.substring(0, index); 
@@ -11,9 +11,66 @@ function fileViewerUrl(url) {
     
     oReq.onload = function (oEvent) { 
       var arrayBuffer = oReq.response; 
-      if (arrayBuffer) { 
-        var byteArray = new Uint8Array(arrayBuffer);  
-      } 
+      // var byteArray = new Uint8Array(arrayBuffer); 
+      var data = new Uint8Array(arrayBuffer); 
+      
+      if (arrayBuffer) {
+                      var buf = new ArrayBuffer(4);
+                      var view = new DataView(buf);
+                      
+                      var sb = data[2];        // size
+                      var bgn = data[10];      // start of data byte
+                      // var sd = 256*(data[15]) + (data[14]); // size
+                      view.setUint8(0, data[15]);
+                      view.setUint8(1, data[14]);
+                      var sd = view.getUint16(0); 
+        
+                      view.setUint8(0, data[19]);
+                      view.setUint8(1, data[18]);
+                      var w = view.getUint16(0); 
+        
+                      view.setUint8(0, data[23]);  
+                      view.setUint8(1, data[22]);
+                      var h = view.getUint16(0);
+                     
+                      var bpp = data[28];   // 
+                      var b = (data[28])/8; // bytes in color
+                   
+                      var cvs = document.getElementById("canvas");
+                      var ctx = cvs.getContext("2d"); 
+                      var imageData = ctx.createImageData(w, h); 
+                     
+                      if (b==3) {
+                      var  i, j, k=0;
+                      for ( i = w*b*h-w*b; i>0; i -=w*b ) {
+                          for ( j = 0; j < w*b; j += b ) {
+                            
+                            imageData.data[k + 0] = data[bgn+2+i+j];  // 255;    // R value
+                            imageData.data[k + 1] = data[bgn+1+i+j]  // 0;  // G value
+                            imageData.data[k + 2] = data[bgn+0+i+j] ;  // 0;    // B value
+                            imageData.data[k + 3] = 255; // data[bgn+3+i+j] ;  //  255;  // A value
+                            k += b+1;
+                           }
+                          }
+                        }
+                   
+                      else if (b==4) {
+                      var i, j, k=0;
+                      for ( i = w*b*h-w*b; i>0; i -=w*b ) {
+                          for ( j = 0; j < w*b; j += b) {
+                            
+                            imageData.data[k + 0] = data[bgn+2+i+j];  // R value
+                            imageData.data[k + 1] = data[bgn+1+i+j];  // G value
+                            imageData.data[k + 2] = data[bgn+0+i+j];  // B value
+                            imageData.data[k + 3] = data[bgn+3+i+j];  // A value
+                            k += b;
+                           }
+                          }
+                        }
+                   
+                         ctx.putImageData(imageData, x, y); 
+      }
+      
     }; 
     
     oReq.send(null);
