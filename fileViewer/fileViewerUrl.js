ac64a5bd3bb53b65681bfe42293dd1498de34e88
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-03 17:57:37 +0530

    Update fileViewerUrl.js

--- fileViewer/fileViewerUrl.js
+++ fileViewer/fileViewerUrl.js
@@ -1,1 +1,18 @@
-
+function fileViewerUrl(url) {  
+  
+  if () {  
+    var oReq = new XMLHttpRequest(); 
+    oReq.open("GET", url, true); 
+    oReq.responseType = "arraybuffer"; 
+    
+    oReq.onload = function (oEvent) { 
+      var arrayBuffer = oReq.response; 
+      if (arrayBuffer) { 
+        var byteArray = new Uint8Array(arrayBuffer);  
+      } 
+    }; 
+    
+    oReq.send(null);
+    
+  }
+}
