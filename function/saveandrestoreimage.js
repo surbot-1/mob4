7b9cfddef3356d7fbdcd24f710fde8637078a69a
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-04 02:40:02 +0530

    Create saveandrestoreimage.js

---
+++ function/saveandrestoreimage.js
@@ -0,0 +1,7 @@
+function saveImage(x, y, w, h) { 
+  var can = document.getElementById("canvas"); 
+  var ctx = can.getContext("2D"); 
+  var imgData = ctx.createImageData(w, h); 
+  imgData = ctx.getImageData(20, 200, 100,100); 
+  ctx.putImageData(20, 400); 
+  }
