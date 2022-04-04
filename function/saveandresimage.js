b11d8edb5c18c42c185949213a04a723e7a9151d
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-04 07:35:46 +0530

    Update saveandresimage.js

--- function/saveandresimage.js
+++ function/saveandresimage.js
@@ -1,11 +1,10 @@
 function saveImage(x, y, w, h) { 
-  alert('1'); 
-  var can = document.getElementById('canvas'); alert('2');
+  var can = document.getElementById('canvas'); 
   var ctx = can.getContext('2d'); alert('3'); 
-  var imgData = ctx.getImageData(x, y, w, h); alert('4'); 
+  // var imgData = ctx.createImageData(w, h); 
+  var imgData = ctx.getImageData(x, y, w, h); 
   var imgBuf = new ArrayBuffer(w*4*h); 
   var imgView = new Uint8Array(imgBuf); 
-  // var imgData = ctx.createImageData(w, h); alert('5'); 
   for (let i=0; i<w*4*h; i+=4) {  
     imgView[i+0] = imgData.data[i+0]; 
     imgView[i+1] = imgData.data[i+1]; 
@@ -31,5 +30,27 @@ function saveImage(x, y, w, h) {
                       
                        }; 
                     
-  }
-                          
+  } 
+
+
+function restoreImage(x, y, w, h) { 
+  var can = document.getElementById('canvas'); 
+  var ctx = can.getContext('2d'); 
+  var imgData = ctx.createImageData(w, h); 
+  // var imgData = ctx.getImageData(x, y, w, h); 
+  var imgBuf = new ArrayBuffer(w*4*h); 
+  var imgView = new Uint8Array(imgBuf); 
+  for (let i=0; i<w*4*h; i+=4) {  
+    imgData.data[i+0] = imgView[i+0]; 
+    imgData.data[i+1] = imgView[i+1]; 
+    imgData.data[i+2] = imgView[i+2]; 
+    imgData.data[i+3] = imgView[i+3]; 
+  } 
+  ctx.putImageData(imgData, x, y); 
+}
+
+
+
+
+
+
