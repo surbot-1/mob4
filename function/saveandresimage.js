fac74717e8bd6140b87f1d83b02185774dfd40d6
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-04 07:44:43 +0530

    Update saveandresimage.js

--- function/saveandresimage.js
+++ function/saveandresimage.js
@@ -11,26 +11,8 @@ function saveImage(x, y, w, h) {
     imgView[i+2] = imgData.data[i+2]; 
     imgView[i+3] = imgData.data[i+3]; 
   }  
-  ctx.putImageData(imgData, 20, 1000); 
-                                
-       /*  var cvs4 = document.getElementById('canvas');
-            var ctx4 = cvs4.getContext('2d'); 
-       
-                  var img4 = new Image();
-                  img4.src = 'logo/Turbo-Snail-icon-_1_.bmp';
-                  img4.onload = function (e4)
-                      { 
-                    
-                      ctx4.drawImage(img4, 20, 400, 128, 128); 
-                      var imgData4 = ctx4.getImageData(20, 400, 128, 128); 
-                      ctx4.putImageData(imgData4, 20, 600); 
-                      
-                      var imgData41 = ctx4.getImageData(20, 200, 128, 128); 
-                      ctx4.putImageData(imgData41, 20, 800); 
-                      
-                       }; */
-                    
-  } 
+  ctx.putImageData(imgData, x, y+400);             
+} 
 
 
 function restoreImage(x, y, w, h) { 
@@ -51,6 +33,22 @@ function restoreImage(x, y, w, h) {
 
 
 
+/*  var cvs4 = document.getElementById('canvas');
+            var ctx4 = cvs4.getContext('2d'); 
+       
+                  var img4 = new Image();
+                  img4.src = 'logo/Turbo-Snail-icon-_1_.bmp';
+                  img4.onload = function (e4)
+                      { 
+                    
+                      ctx4.drawImage(img4, 20, 400, 128, 128); 
+                      var imgData4 = ctx4.getImageData(20, 400, 128, 128); 
+                      ctx4.putImageData(imgData4, 20, 600); 
+                      
+                      var imgData41 = ctx4.getImageData(20, 200, 128, 128); 
+                      ctx4.putImageData(imgData41, 20, 800); 
+                      
+                       }; */
 
 
 
