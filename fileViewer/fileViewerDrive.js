150935951ff3863ae108c95a92696bb7e7ac138e
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-05 20:54:44 +0530

    Update fileViewerDrive.js

--- fileViewer/fileViewerDrive.js
+++ fileViewer/fileViewerDrive.js
@@ -1,7 +1,7 @@
 function fileViewerDrive(x,y,filedir,cluno,size) {  
  /* var filename=""; 
   var fileext=""; 
-  fileext=filedir.substring(8); 
+  fileext=filedir.substring(8,11); 
   
   if (fileext=="txt") {fileViewerDriveTxt(x,y,filedir,cluno,size);} 
 */
@@ -10,12 +10,12 @@ function fileViewerDrive(x,y,filedir,cluno,size) {
 function fileViewerDriveTxt(x,y,filedir,cluno,size) { alert('1');
   var filename=""; 
   var fileext=""; 
-  fileext=filedir.substring(8); 
-  
+  fileext=filedir.substring(8,11); 
+  alert(fileext);
   if (fileext=="txt") { 
     var text=""; 
     for (let i=0; i<size; i++) {
-      text+=ascChar(driveView[clust0+cluno*8*512+i]); 
+      text+=ascChar((driveView[clust0+cluno*8*512+i])); 
     } 
     writeStr(x,y,480,128"ubuntubold",text);
   }
