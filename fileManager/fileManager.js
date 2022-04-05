29a3c7189866a899679776b09af8871497e5bbc3
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-06 01:40:00 +0530

    Update fileManager.js

--- fileManager/fileManager.js
+++ fileManager/fileManager.js
@@ -1,15 +1,16 @@
 function fileManager(op, file, blob, size) { alert('1');
   
   if (op=="create") { 
-    var filename=file.substring(0,(file.indexOf(".")));  
-    var fileext=file.substring((file.indexOf("."))+1); 
+    var index = file.indexOf(".");
+    var filename=file.substring(0, index);  
+    var fileext=file.substring(index+1, index+4); 
     var filercv=""; 
-    for (let i=0; i<11; i++) { 
+   /* for (let i=0; i<11; i++) { 
       if (i>=0 && i<filename.length) {filercv+=filename.charAt(i);} 
       if (i>=filename.length && i<8) {filercv+=" ".charAt(0);} 
       if (i>=8 && i<11) {filercv+=fileext.charAt(i-8);} 
     } alert('2');
-   /* var cluho=[]; 
+    var cluho=[]; 
     var clulo=[]; 
     var fsize=[]; 
     cluho[0]=cluno&0x00FF0000;     // lsb 
