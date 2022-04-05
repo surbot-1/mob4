b45c8b515a1afa9d11c75907e6380131b8b2e4a9
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-05 22:45:25 +0530

    Update fileViewerDrive.js

--- fileViewer/fileViewerDrive.js
+++ fileViewer/fileViewerDrive.js
@@ -11,7 +11,6 @@ function fileViewerDriveTxt(x,y,filedir,cluno,size) {
   var filename=""; 
   var fileext=""; 
   fileext=filedir.substring(8,11); 
-  alert(fileext); 
   if (fileext=="txt") { 
     for (let i=0; i<size; i++) { 
        text += ascChar(driveView[clust0+cluno*8*512+i]); 
