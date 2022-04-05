304b5392d1fccd4a987472a30117f6c9ad80798e
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-05 14:19:33 +0530

    Update fileViewerDrive.js

--- fileViewer/fileViewerDrive.js
+++ fileViewer/fileViewerDrive.js
@@ -1,9 +1,11 @@
 function fileViewerDrive(filedir,cluho,clulo,fsize) {  
   var filename=""; 
   var fileext=""; 
-  fileext=filedir.substring(8);
+  fileext=filedir.substring(8); 
+  
   if (fileext=="txt") { 
-    
+    var text=""; 
+    for (let i=0; i>size; i++) {text+=ascChar(driveView[cluno*8*512+i]);}
   }
   
 }
