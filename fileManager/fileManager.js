b91c62a708f7b61fb35b0bdaea8d0f4303ecc53b
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-07 23:53:19 +0530

    Update fileManager.js

--- fileManager/fileManager.js
+++ fileManager/fileManager.js
@@ -117,23 +117,7 @@ function fileManager(op, file, buf, size) {
   }
   
 
-function fileManagerSaveUrl(url) { 
-    var index = url.indexOf('.'); 
-    var filename = url.substring(0, index); 
-    var fileext = url.substring(index+1, index+4); 
-    var file = url; 
-   
-    var oReq = new XMLHttpRequest(); 
-    oReq.open("GET", url, true); 
-    oReq.responseType = "arraybuffer"; 
-    
-    oReq.onload = function (oEvent) { 
-      var buf = oReq.response; 
-      var size = buf.byteLength; 
-      function fileManagerSave(file, buf, size)
-     }; 
-    oReq.send(null); 
-}
+
  
 function fileManagerSaveStorage(url) { 
     var index = url.indexOf('.'); 
