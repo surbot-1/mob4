b15dc3a9963511e7e2f46434f9c53e39f35490b2
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-09 02:10:41 +0530

    Update fileManager.js

--- fileManager/fileManager.js
+++ fileManager/fileManager.js
@@ -1,8 +1,7 @@
 function fileManager(op, file, buf, size) { 
   if (op=="create") {fileManagerCreate(file, buf, size);} 
   if (op=="open") {fileManagerOpen(buf, size, file);} 
-}
-  alert('1');
+} 
   
   function fileManagerCreate(file, buf, size) {
     var index = file.indexOf(".");
@@ -13,26 +12,26 @@ function fileManager(op, file, buf, size) {
       if (i>=0 && i<filename.length) {filercv+=filename.charAt(i);} 
       if (i>=filename.length && i<8) {filercv+=" ".charAt(0);} 
       if (i>=8 && i<11) {filercv+=fileext.charAt(i-8);} 
-    } alert(filercv.length); alert(fileext); alert(filercv.charAt(8));
+    } 
     var cluho=[]; 
     var clulo=[]; 
     var fsize=[]; 
     var fat=fatno; 
-    var clust=cluno; alert(clust); 
+    var clust=cluno; 
     cluho[0]=clust&0x00FF0000; 
-    cluho[0]=cluho[0]>>16;      alert(cluho[0]);
+    cluho[0]=cluho[0]>>16;      
     cluho[1]=clust&0xFF000000; 
-    cluho[1]=cluho[1]>>24;      alert(cluho[1]);
-    clulo[0]=clust&0x000000FF;  alert(clulo[0]);
+    cluho[1]=cluho[1]>>24;      
+    clulo[0]=clust&0x000000FF;  
     clulo[1]=clust&0x0000FF00;
-    clulo[1]=clulo[1]>>8;       alert(clulo[1]);
+    clulo[1]=clulo[1]>>8;       
     fsize[0]=size&0x000000FF; 
     fsize[1]=size&0x0000FF00; 
     fsize[1]=fsize[1]>>8; 
     fsize[2]=size&0x00FF0000; 
     fsize[2]=fsize[2]>>16; 
     fsize[3]=size&0xFF000000; 
-    fsize[3]=fsize[3]>>24;      alert(fsize);
+    fsize[3]=fsize[3]>>24;      
     alert ('3');
       for (i=0; i<32; i++) { 
       if (i>=0 && i<11) {driveView[dirct0+dirno*32+i]=filercv.charCodeAt(i);} 
@@ -80,14 +79,12 @@ function fileManager(op, file, buf, size) {
     var filedir=""; 
     var cluho=[]; 
     var clulo=[]; 
-    var fsize=[]; 
-    // var x=buf; 
-    // var y=size; alert('10');
+    var fsize=[];  
     for (let i=0; i<11; i++) { 
       if (i>=0 && i<filename.length) {filercv+=filename.charAt(i);} 
       if (i>=filename.length && i<8) {filercv+=" ".charAt(0);} 
       if (i>=8 && i<11) {filercv+=fileext.charAt(i-8);} 
-    } alert('20'); alert(filercv); 
+    } 
     var b=false; 
     for (let i=0; i<dirno; i++) { 
       for (let j=0; j<32; j++) { 
@@ -95,23 +92,23 @@ function fileManager(op, file, buf, size) {
         if (j>=20 && j<22) {cluho[j-20]=driveView[dirct0+i*32+j];} 
         if (j>=26 && j<28) {clulo[j-26]=driveView[dirct0+i*32+j];} 
         if (j>=28 && j<32) {fsize[j-28]=driveView[dirct0+i*32+j];} 
-      } alert(filedir);
-      if (filedir==filercv) {b=true; alert(b); break;} 
+      } 
+      if (filedir==filercv) {b=true; break;} 
       else {filedir="";}
     } 
-    if (b) { alert(filedir); 
+    if (b) { 
       var buf = new ArrayBuffer(4); 
       var view = new DataView(buf); 
       view.setUint8(0,cluho[1]); 
       view.setUint8(1,cluho[0]); 
       view.setUint8(2,clulo[1]); 
       view.setUint8(3,clulo[0]); 
-      var clust=view.getUint32(0);  alert(clust); 
+      var clust=view.getUint32(0);   
       view.setUint8(0,fsize[3]); 
       view.setUint8(1,fsize[2]); 
       view.setUint8(2,fsize[1]); 
       view.setUint8(3,fsize[0]); 
-      var size=view.getUint32(0);  alert(size); 
+      var size=view.getUint32(0);   
       fileViewerDrive(x,y,filedir,clust,size); 
     } 
   }
