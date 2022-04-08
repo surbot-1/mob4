73dea9b146269e07f9638414126a48aea53a7eee
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-08 09:13:11 +0530

    Update fileViewerDrive.js

--- fileViewer/fileViewerDrive.js
+++ fileViewer/fileViewerDrive.js
@@ -50,7 +50,7 @@ function fileViewerDriveBmp(x,y,filedir,clust,size) {
           j++; 
         }
       } alert(data); 
-     var buf=data.buffer; alert(Uint8Array(buf));
+     var buf=data.buffer; // alert(Uint8Array(buf));
     // var blob = new Blob([data.buffer]); 
     // var buf = await blob.arrayBuffer; alert(buf.byteLength);
     drawImageBmp(x, y, buf);
