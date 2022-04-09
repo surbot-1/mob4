4a27bb5d84e3a44bc237d08f669f9debf23b6ca2
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-09 19:48:49 +0530

    Update message.js

--- function/message.js
+++ function/message.js
@@ -1,8 +1,21 @@
 function message() { 
-  var kstr=readKeypad(0,1664,0); 
+  var kstr=readKeypad(0,1664,ktype); 
   if (kstr=="SIFT") { 
-  } else if (kstr=="BS") { 
+    if (ktype==0 || ktype==2) { 
+      ktype++; 
+      drawdKeypad(0,1664,ktype);
+    } 
+    if (ktype==1 || ktype==3) { 
+      ktype--; 
+      drawKeypad(0,1664,ktype); 
+    }
   } else if (kstr=="?123") { 
+    ktype=2; 
+    drawKeypad(0,1664,ktype);
+  } else if (kstr=="ABC") { 
+    ktype=0; 
+    drawKeypad(0,1664,ktype); 
+  } else if (kstr=="BS") { 
   } else if (kstr=="SPACE") { 
     writeChar(16+ci,1448+cj,"font2448"," ");
     ci+=24; 
@@ -11,7 +24,7 @@ function message() {
     msgPtri++; 
   } else if (kstr=="ENTER") { 
   } else { 
-    var kchar=readKeypad(0,1664,0); 
+    var kchar=readKeypad(0,1664,ktype); 
     writeChar(16+ci,1448+cj,"font2448",kchar); 
     ci+=24; 
     if (ci>=24*16) {ci=0; cj=0;} 
