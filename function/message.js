bf2fa48e4aed007b68566fe076577e3c1a678474
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-09 19:20:45 +0530

    Update message.js

--- function/message.js
+++ function/message.js
@@ -1,9 +1,10 @@
 function message() { 
   var kstr=readKeypad(0,1664,0); 
-  if (kstr=="SHIFT") { 
+  if (kstr=="SIFT") { 
   } else if (kstr=="BS") { 
   } else if (kstr=="?123") { 
   } else if (kstr=="SPACE") { 
+    writeChar(16+ci,1448+cj,"font2448"," ");
     ci+=24; 
     if (ci>=24*16) {ci=0; cj=0;} 
     msgView[msgPtrj+msgPtri]=" ".charCodeAt(0); 
