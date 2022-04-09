b23f5b94c6801a6f7c4dc9a9bf86acfa9d9590ba
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-09 16:45:47 +0530

    Update attachment.js

--- function/attachment.js
+++ function/attachment.js
@@ -1,1 +1,34 @@
+function attachment() { 
+  
+  var x; 
+         openFile(); 
+function openFile() {
+  x = document.createElement("INPUT");
+  x.setAttribute("type", "file"); 
+  x.setAttribute("id", "file"); 
+  // x.setAttribute("style", "display:none"); 
+  // x.style.display='none';  
+  document.body.appendChild(x); 
+  // document.getElementById("file").click(); 
+  var element = document.getElementById("file");
+  element.addEventListener("change", readFile);
+}  
+	
+function readFile(e) { 
+  var file = e.target.files[0];
+  if (!file) {return;}
+  var reader = new FileReader(); 
+  reader.onload = function(e) { 
+     var buf = e.target.result; 
+     var view = new Uint8Array(buf); 
+     var size = buf.byteLength; 
+     drawImageBmp(540, 512, buf, size); 
+     fileManagerSave("image.bmp",buf,size); 
+     fileManagerOpen(540,800,"image.bmp"); 
+     document.body.removeChild(x); 
+  }
+  reader.readAsArrayBuffer(file)
+} 
+  
+}
 
