e4dff79137b04f44905d37264ff91fb2c3f0535f
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-09 19:12:32 +0530

    Update message.js

--- function/message.js
+++ function/message.js
@@ -1,1 +1,20 @@
-
+function message() { 
+  var kstr=readKeypad(0,1664,0); 
+  if (kstr=="SHIFT") { 
+  } else if (kstr=="BS") { 
+  } else if (kstr=="?123") { 
+  } else if (kstr=="SPACE") { 
+    ci+=24; 
+    if (ci>=24*16) {ci=0; cj=0;} 
+    msgView[msgPtrj+msgPtri]=" ".charCodeAt(0); 
+    msgPtri++; 
+  } else if (kstr=="ENTER") { 
+  } else { 
+    var kchar=readKeypad(0,1664,0); 
+    writeChar(16+ci,1448+cj,"font2448",kchar); 
+    ci+=24; 
+    if (ci>=24*16) {ci=0; cj=0;} 
+    msgView[msgPtrj+msgPtri]=kchar.charCodeAt(0); 
+    msgPtri++; 
+  }
+} 
