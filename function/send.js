11ef371ec283171d6ca2ee07f132998a5a8e7740
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-10 16:19:44 +0530

    Update send.js

--- function/send.js
+++ function/send.js
@@ -6,7 +6,7 @@ function sendMessage () {
   } 
   msgPtr++; 
   send(); 
-} 
+}  
 
 function send () { 
   var minfo = msgView[(msgPtr-1)*512+0]; 
@@ -14,7 +14,7 @@ function send () {
   var str = ""; 
   for (let i=0; i<msize; i++) { 
     str += ascChar(msgView[(msgPtr-1)*512+32+i]); 
-  } 
+  }  
   writecStr(540,200,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],str);
 }
 
