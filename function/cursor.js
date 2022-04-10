3aff43f44a67449dbaa9724705c28040ddc02fb4
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-10 10:59:39 +0530

    Update cursor.js

--- function/cursor.js
+++ function/cursor.js
@@ -2,10 +2,10 @@ function writeCursor() {
         var cvs = document.getElementById("canvas");
         var ctx = cvs.getContext('2d'); 
         ctx.fillStyle = "blue"; 
-        ctx.fillRect(cx, cy, 4, 48); 
+        ctx.fillRect(ci, cj, 4, 48); 
 	setTimeout( function() { 
 	ctx.fillStyle = "white"; 
-        ctx.fillRect(cx, cy, 4, 48); 
+        ctx.fillRect(ci, cj, 4, 48); 
         }, 0500); 
       }	 
 
@@ -13,16 +13,16 @@ function showCursor() {
         var cvs = document.getElementById("canvas");
         var ctx = cvs.getContext('2d');
         ctx.fillStyle = "blue";
-        ctx.fillRect(cx, cy, 4, 48);
+        ctx.fillRect(ci, cj, 4, 48);
         }
 	
 function updateCursor() {
         var cvs = document.getElementById("canvas");
         var ctx = cvs.getContext('2d');
         ctx.fillStyle = "white";
-        ctx.fillRect(cxp, cyp, 4, 48);
+        ctx.fillRect(cip, cjp, 4, 48);
 	ctx.fillStyle = "blue";
-        ctx.fillRect(cx, cy, 4, 48);
+        ctx.fillRect(ci, cj, 4, 48);
         }
 	
 function clrCursor() {
@@ -30,5 +30,5 @@ function clrCursor() {
         var ctx = cvs.getContext('2d');
         clearInterval(cursor);
         ctx.fillStyle = "white";
-        ctx.fillRect(cx, cy, 4, 48);
+        ctx.fillRect(ci, cj, 4, 48);
         }
