105f4303128f2e167d60f4c17eccd2c91810108f
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-10 16:35:26 +0530

    Update message.js

--- function/message.js
+++ function/message.js
@@ -21,28 +21,28 @@ function message() {
     } else { 
       if (ci==0) {ci=24*23;} else {ci-=24;} 
       writeChar(16+ci,1448+cj,"font2448"," "); 
-      cpr--; 
+      ptr--; 
       msgByte[0]=0x00; 
-      msgByte[28]=cpr; 
+      msgByte[28]=ptr; 
     }
   } else if (kstr=="SPACE") { 
     writeChar(16+ci,1448+cj,"font2448"," ");
     ci+=24; 
     if (ci>=24*24) {ci=0; cj=0;} 
-    msgByte[32+cpr]=" ".charCodeAt(0); 
-    cpr++; 
+    msgByte[32+ptr]=" ".charCodeAt(0); 
+    ptr++; 
     msgByte[0]=0x00;
-    msgByte[28]=cpr; 
+    msgByte[28]=ptr; 
   } else if (kstr=="ENTER") { 
   } else { 
     var kchar=readKeypad(0,1664,ktype); 
     writeChar(16+ci,1448+cj,"font2448",kchar); 
     ci+=24; 
     if (ci>=24*24) {ci=0; cj=0;} 
-    msgByte[32+cpr]=kchar.charCodeAt(0); 
-    cpr++; 
+    msgByte[32+ptr]=kchar.charCodeAt(0); 
+    ptr++; 
     msgByte[0]=0x00;
-    msgByte[28]=cpr; 
+    msgByte[28]=ptr; 
   } 
   writeCursor(16,1448); 
   showCursor(16,1448);
