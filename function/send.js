702f0f2da3cca15c3c9e6bc21e51dedbeb0b34da
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-11 01:37:55 +0530

    Update send.js

--- function/send.js
+++ function/send.js
@@ -40,7 +40,7 @@ function send() {
     x=600; y=200; 
     ctx.fillStyle = "rgba(128, 128, 240, 1.0)"; // blue 
     ctx.fillRect(x, y, w+32, h+32+32); 
-    writecStr(x+16,y+16,480-48,128,"ubuntubold",[0,0,0,255],[128,128,240,255],str);
+    writecStr(x+16,y+16,480-48,128,"ubuntubold",[0,0,0,255],[128,128,240,255],str); 
   } else if(minfo==1) { 
     x=16; y=328; 
     ctx.fillStyle = "rgba(255, 255, 255, 1.0)"; // blue 
