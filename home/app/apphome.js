79cceb974fd4d15b43b886feed2fb80f3ffe81d0
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-12 14:23:21 +0530

    Update apphome.js

--- home/app/apphome.js
+++ home/app/apphome.js
@@ -32,11 +32,11 @@ fileViewerUrl(920, 1896, "icon/add-icon-128.bmp");
               } else if (x>0 && x<1080 && y>144*1 && y<144*2) { 
 		      clearInterval(timer); appHomeChatsChatbot(); 
               } else if (x>0 && x<1080 && y>144*2 && y<144*3) { 
-		      clearInterval(timer); appHomeChatsUser1(); 
+		      clearInterval(timer); appHomeChatsUser("Yash Shiv"); 
               } else if (x>0 && x<1080 && y>144*3 && y<144*4) { 
-		      clearInterval(timer); appHomeChatsUser("user1"); 
+		      clearInterval(timer); appHomeChatsUser("Jitendra"); 
               } else if (x>0 && x<1080 && y>144*4 && y<144*5) { 
-		      clearInterval(timer); appHomeChatsUser("user2"); 
+		      // clearInterval(timer); appHomeChatsUser(""); 
               } else if (x>0 && x<1080 && y>160*5 && y<160*5+144) { 
 	      } else if (x>0 && x<1080 && y>160*6 && y<160*6+144) { 
 	      } else if (x>0 && x<1080 && y>160*7 && y<160*7+144) { 
