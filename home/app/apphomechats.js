1d83f3a56831489e4e727f8c770274bc7856a299
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-12 14:29:08 +0530

    Update apphomechats.js

--- home/app/apphomechats.js
+++ home/app/apphomechats.js
@@ -11,14 +11,6 @@ fileViewerUrl(952, 48, "icon/telephone-icon-48.bmp");
 	
 ctx.fillStyle = "rgba(240, 240, 240, 1.0)"; 
 ctx.fillRect(0, 144, 1080, 2032); 
- /*  
-fileViewerUrl(64, 144*1+8, "icon/business-woman-icon-96.bmp"); 
-fileViewerUrl(64, 144*2+8, "icon/business-man-icon-96.bmp"); 
-fileViewerUrl(64, 144*3+8, "icon/business-man-icon-96.bmp"); 
-writecStr(200,144*1+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"sAy"); 
-writecStr(200,144*2+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"Yash Shiv"); 
-writecStr(200,144*3+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"Jitendra"); 
-*/ 
 ctx.fillStyle = "rgba(0,0,128,1.0)"; // blue
 ctx.fillRect(0, 2048, 920, 128); 
 ctx.fillStyle = "rgba(255,255,255,1.0)"; // white
@@ -122,7 +114,63 @@ bot=true;
 	timer = setInterval(check, 0100); 
 } 
 
-function appHomeChatsUser1Chat() {  
+function appHomeChatsUser(user) {  
+var cnv = document.getElementById("canvas"); 
+var ctx = cnv.getContext('2d'); 
+ctx.fillStyle = "rgba(0, 0, 128, 1.0)"; // blue
+ctx.fillRect(0, 0, 1080, 144); 
+fileViewerUrl(64, 8, "icon/business-man-icon-128.bmp");  
+fileViewerUrl(824, 48, "icon/video-icon-48.bmp");  
+fileViewerUrl(952, 48, "icon/telephone-icon-48.bmp"); 
+  writecStr(200,48,144,96,"ubuntubold",[255,255,255,255],[0,0,128,255],user); 
+  writecStr(200,80,144,96,"ubuntufont",[255,255,255,255],[0,0,128,255],"online"); 
+	
+ctx.fillStyle = "rgba(240, 240, 240, 1.0)"; 
+ctx.fillRect(0, 144, 1080, 2032); 
+ctx.fillStyle = "rgba(0,0,128,1.0)"; // blue
+ctx.fillRect(0, 2048, 920, 128); 
+ctx.fillStyle = "rgba(255,255,255,1.0)"; // white
+ctx.fillRect(0+8, 2048+8, 920-16, 128-16);  
+fileViewerUrl(680, 2080, "icon/attachment-icon-64.bmp"); 
+fileViewerUrl(808, 2080, "icon/camera-icon-64.bmp"); 
+fileViewerUrl(936, 2048, "icon/microphone-icon-128.bmp"); 
+	
+	
+	var timer;
+	function check() { 
+	var x = touchx;  var y = touchy; 
+	var tend = touch;  
+           if (tend == 3) { touch = 0;
+              if (x>0 && x<360*1 && y>0 && y<144) { 
+              } else if (x>360*1 && x<360*2 && y>0 && y<144) { 
+              } else if (x>360*2 && x<360*3 && y>0 && y<144) { 
+              } else if (x>0 && x<1080 && y>144*1 && y<144*2) { 
+		    //  clearInterval(timer); appHomeChatsUser1(); 
+              } else if (x>0 && x<1080 && y>144*2 && y<144*3) { 
+              } else if (x>0 && x<1080 && y>144*3+8 && y<144*3+8+128) { 
+              } else if (x>0 && x<1080 && y>160*4 && y<160*4+144) { 
+              } else if (x>0 && x<1080 && y>160*5 && y<160*5+144) { 
+	      } else if (x>0 && x<1080 && y>160*6 && y<160*6+144) { 
+	      } else if (x>0 && x<1080 && y>160*7 && y<160*7+144) { 
+	      } else if (x>0 && x<1080 && y>160*8 && y<160*8+144) { 
+	      } else if (x>0 && x<1080 && y>160*9 && y<160*9+144) { 
+	      } else if (x>0 && x<1080 && y>160*10 && y<160*10+144) { 
+	      } else if (x>0 && x<680 && y>2048 && y<2176) { 
+		      clearInterval(timer); appHomeChatsUserChat(user); 
+	      } else if (x>680 && x<680+64 && y>2080 && y<2080+64) { 
+		      /* clearInterval(timer); */ attachment(); 
+	      } else if (x>808 && x<808+64 && y>2080 && y<2080+64) { 
+		      /* clearInterval(timer); */ camera(); 
+	      } else if (x>936 && x<936+128 && y>2048 && y<2048+128) { 
+		      /* clearInterval(timer); */ mic(); 
+	      }
+		      
+           } 
+	}
+	timer = setInterval(check, 0100); 
+} 
+
+function appHomeChatsUserChat(user) {  
 var cnv = document.getElementById("canvas"); 
 var ctx = cnv.getContext('2d'); 
 ctx.fillStyle = "rgba(0, 0, 128, 1.0)"; // blue
@@ -130,7 +178,7 @@ ctx.fillRect(0, 0, 1080, 144);
 fileViewerUrl(64, 8, "icon/business-woman-icon-128.bmp");  
 fileViewerUrl(824, 48, "icon/video-icon-48.bmp"); 
 fileViewerUrl(952, 48, "icon/telephone-icon-48.bmp"); 
-  writecStr(200,48,144,96,"ubuntubold",[255,255,255,255],[0,0,128,255],"Yash Shiv"); 
+  writecStr(200,48,144,96,"ubuntubold",[255,255,255,255],[0,0,128,255],user); 
   writecStr(200,80,144,96,"ubuntufont",[255,255,255,255],[0,0,128,255],"online");
 	
 ctx.fillStyle = "rgba(240, 240, 240, 1.0)"; 
