6548a0686698135a5113396cf0367948e3bdc9bc
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-12 20:12:53 +0530

    Update firebase.js

--- function/firebase.js
+++ function/firebase.js
@@ -21,13 +21,13 @@ function readAppMessage(user) {
     var ip = snapshot.child("Ip").val(); 
     // snapshot.forEach(function(element) { 
     for (let i=0; i<name.length; i++) {
-       usrView[1+i]=name.charCodeAt(i); 
+       usrByte[1+i]=name.charCodeAt(i); 
 	  } 
     for (let i=0; i<message.length; i++) {
-       usrView[32+i]=message.charCodeAt(i); 
+       usrByte[32+i]=message.charCodeAt(i); 
 	  } 
-    usrView[0]=0x02; 
-    usrView[28]=message.length; 
+    usrByte[0]=0x02; 
+    usrByte[28]=message.length; 
     sendUserMessage(); 
     
     /* var obj = { 
