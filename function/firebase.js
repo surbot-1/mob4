53c0aa8695ccaaa4cd50a9e170df154e3451612f
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-13 09:38:00 +0530

    Update firebase.js

--- function/firebase.js
+++ function/firebase.js
@@ -29,15 +29,28 @@ function readAppMessage(user) {
     usrByte[0]=0x02; 
     usrByte[28]=message.length;  
     sendUserMessage(); 
-    /* var obj = { 
-      msgid: msgid, 
-      name: name, 
-      massage: message, 
-      status: status, 
-      date: date, 
-      ip: ip 
-    }; 
-    return obj; */ 
+  }); 
+} 
+
+function readSenderMessage(user) { 
+  var ref = firebase.database().ref("App").child(user); 
+  ref.once("value", function(snapshot) { 
+    var msgid = snapshot.child("Msgid").val(); 
+    var name = snapshot.child("Name").val(); 
+    var message = snapshot.child("Message").val(); 
+    var status = snapshot.child("Status").val(); 
+    var time = snapshot.child("Time").val(); 
+    var ip = snapshot.child("Ip").val(); 
+    // snapshot.forEach(function(element) { 
+    for (let i=0; i<name.length; i++) {
+       sndByte[1+i]=name.charCodeAt(i); 
+    } 
+    for (let i=0; i<message.length; i++) {
+       sndByte[32+i]=message.charCodeAt(i); 
+    } 
+    sndByte[0]=0x03; 
+    sndByte[28]=message.length;  
+    sendSenderMessage(); 
   }); 
 } 
 
