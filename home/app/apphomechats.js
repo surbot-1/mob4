24bdfe63fef9991468699f15fba81abbc2129d7c
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-13 06:56:42 +0530

    Update apphomechats.js

--- home/app/apphomechats.js
+++ home/app/apphomechats.js
@@ -195,6 +195,17 @@ drawKeypad(0,1664,ktype);
 showCursor(16,1448); 
 bot=false; 
 	
+var tmr; var midp; 
+function rcv { 
+  var mid = getAppMessage(user+"To"+sender,"Msgid"); 
+  if (mid!=midp) { 
+  readAppMessage(user+"To"+sender); 
+  sendUserMessage(); 
+  midp=mid; 
+  }  
+} 
+tmr = setInterval(rcv, 0100); 
+	
 	var timer;
 	function check() { 
 	var x = touchx;  var y = touchy; 
