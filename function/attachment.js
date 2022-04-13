a4ad56036a3ed36c542de82c6ed47d3a70336229
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-13 14:08:17 +0530

    Update attachment.js

--- function/attachment.js
+++ function/attachment.js
@@ -17,12 +17,22 @@ function openFile() {
 	
 function readFile(e) { 
   var file = e.target.files[0]; 
+  if (!file) {return;} 
   var name = file.name; 
   var index = name.indexOf("."); 
   var fname = name.substring(0,index);
-  var fext = name.substring(index+1,index+4); 
-  if (!file) {return;}
-  var reader = new FileReader(); 
+  var fext = name.substring(index+1,index+4);  
+  if (fext=="jpg"||fext=="jpe"||fext=="png"fext=="bmp") { 
+       var reader = new FileReader(); 
+       var image = new Image(); 
+       image.src=URL.createObjectURL(file); 
+       image.onload = function (e) { 
+    ctx.drawImage(image,0,200);
+    URL.revokeObjectURL(image.src); 
+    document.body.removeChild(x); 
+    }; 
+  }
+ /* var reader = new FileReader(); 
   reader.onload = function(e) { 
      var buf = e.target.result; 
      var view = new Uint8Array(buf); 
@@ -30,9 +40,9 @@ function readFile(e) {
      drawImageBmp(540, 512, buf, size); 
      fileManagerSave("image.bmp",buf,size); 
      // fileManagerOpen(540,800,"image.bmp"); 
-     document.body.removeChild(x); 
-  }
-  reader.readAsArrayBuffer(file)
+     document.body.removeChild(x);  
+  }; 
+  reader.readAsArrayBuffer(file) */ 
 } 
-document.getElementById("file").click(
+document.getElementById("file").click(); 
 }
