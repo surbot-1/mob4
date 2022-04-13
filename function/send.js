0344e6622ece4d8b7ace53d04badd4801b75174c
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-13 12:24:51 +0530

    Update send.js

--- function/send.js
+++ function/send.js
@@ -47,7 +47,7 @@ function send() {
   var time = getTime("12h"); 
   var x=0; var y=0; 
   if (minfo==0) { 
-    x=600; y=144+1264-(h+64); 
+    x=600; y=1408-(h+64)-16; 
     ctx.fillStyle = "rgba(200, 240, 200, 1.0)"; // blue 
     ctx.fillRect(x, y, w+32, h+32+32); 
     writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[200,240,200,255],msgstr); 
@@ -57,19 +57,19 @@ function send() {
     setTimeout(() => {msgStatus(x+360,y+h+24,"dlvd");},3000); 
     setTimeout(() => {msgStatus(x+360,y+h+24,"seen");},4000);  
   } else if(minfo==1) { 
-    x=16; y=144+1264-(h+64); 
+    x=16; y=1408-(h+64)-16; 
     ctx.fillStyle = "rgba(255, 255, 255, 1.0)"; // white 
     ctx.fillRect(x, y, w+32, h+32+32); 
     writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[255,255,255,255],msgstr); 
     writecStr(x+288,y+h+24,432,128,"ubuntufont",[0,0,0,255],[255,255,255,255],time); 
   } else if(minfo==2) { 
-    x=16; y=144+1264-(h+64); 
+    x=16; y=1408-(h+64)-16; 
     ctx.fillStyle = "rgba(255, 255, 255, 1.0)"; // white 
     ctx.fillRect(x, y, w+32, h+32+32); 
     writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[255,255,255,255],msgstr); 
     writecStr(x+288,y+h+24,432,128,"ubuntufont",[0,0,0,255],[255,255,255,255],time); 
   } else if(minfo==3) { 
-    x=600; y=144+1264-(h+64); 
+    x=600; y=1408-(h+64)-16; 
     ctx.fillStyle = "rgba(200, 240, 200, 1.0)"; // blue  
     ctx.fillRect(x, y, w+32, h+32+32); 
     writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[200,240,200,255],msgstr); 
