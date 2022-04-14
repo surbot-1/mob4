9e274d431912ba1dfb5c6d54a7fa9a4587ed866f
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-14 12:21:38 +0530

    Update apphomechats.js

--- home/app/apphomechats.js
+++ home/app/apphomechats.js
@@ -23,7 +23,7 @@ fileViewerUrl(936, 2048, "icon/microphone-icon-128.bmp");
 var ax=0; var ay=0; var aw=1080; var ah=0; alert(aw);
 if (appPtr<1880) {ay=144+1880-appPtr; ah=appPtr;} 
 else if (appPtr>=1880) {ay=144; ah=1880;} alert(appPtr); 
-var imgData = ctx.createImageData(aw, ah);   alert('4'); 
+var imgData = ctx.createImageData(1080, 1880);    alert('4'); 
 appPtri=0; appPtrf=ah; alert(appPtr); alert('4'); 
 for (let i=0; i<aw*4*ah; i+=4) { 
     imgData.data[i+0]=appView[appPtri+i+0]; 
