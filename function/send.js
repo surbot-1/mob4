31e02dc170893f505e08c237106fe92f5e5444d9
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-15 08:02:23 +0530

    Update send.js

--- function/send.js
+++ function/send.js
@@ -18,6 +18,74 @@ function sendMessage() {
   if (bot) {setTimeout(replyChatbot, 5000);}
 }  
 
+function send1() { 
+  var cnv = document.getElementById("canvas"); 
+  var ctx = cnv.getContext('2d'); 
+  // var imgBuf = new ArrayBuffer(4*1080*2032); 
+  // var imgData = new Uint8Array(imgBuf); 
+  var buf = new ArrayBuffer(4); 
+  var view = new DataView(buf); 
+  var minfo = msgView[(msgPtr-1)*512+0]; 
+  var msize = msgView[(msgPtr-1)*512+28]; 
+  view.setUint8(0, msgView[(msgPtr-1)*512+17]); 
+  view.setUint8(1, msgView[(msgPtr-1)*512+16]);  
+  var w = view.getUint16(0); 
+  view.setUint8(0, msgView[(msgPtr-1)*512+19]); 
+  view.setUint8(1, msgView[(msgPtr-1)*512+18]);  
+  var h = view.getUint16(0); 
+  var imgData = ctx.createImageData(1080, 1264-32-(h+64)); 
+  imgData = ctx.getImageData(0,144+h+64+8,1080,1264-32-(h+64)); 
+  ctx.putImageData(imgData,0,144); 
+  ctx.fillStyle = "rgba(240, 240, 240, 1.0)"; // lightgray
+  ctx.fillRect(0, 1408-(h+64)-32, 1080, h+64+32); 
+  var msgstr = ""; 
+  for (let i=0; i<msize; i++) { 
+    msgstr += ascChar(msgView[(msgPtr-1)*512+32+i]); 
+  } 
+  var x=0; var y=0; 
+  if (minfo==0) { 
+    x=600; y=1408-(h+64)-32; 
+    ctx.fillStyle = "rgba(200, 240, 200, 1.0)"; // blue 
+    ctx.fillRect(x, y, w+32, h+32+32); 
+    var time = getTime("12h"); 
+    writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[200,240,200,255],msgstr); 
+    writecStr(x+168,y+h+24,432,128,"ubuntufont",[0,0,0,255],[200,240,200,255],time); 
+    // setTimeout(() => {msgStatus(x+360,y+h+24,"send");},1000); 
+    setTimeout(() => {msgStatus(x+360,y+h+24,"sent");},2000); 
+    setTimeout(() => {msgStatus(x+360,y+h+24,"dlvd");},3000); 
+    setTimeout(() => {msgStatus(x+360,y+h+24,"seen");},4000);  
+  } else if(minfo==1) { 
+    var time = getTime("12h"); 
+    x=16; y=1408-(h+64)-32; 
+    ctx.fillStyle = "rgba(255, 255, 255, 1.0)"; // white 
+    ctx.fillRect(x, y, w+32, h+32+32); 
+    writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[255,255,255,255],msgstr); 
+    writecStr(x+288,y+h+24,432,128,"ubuntufont",[0,0,0,255],[255,255,255,255],time); 
+  } else if(minfo==2) { 
+    x=16; y=1408-(h+64)-32;
+    var time = getTime("12h"); 
+    ctx.fillStyle = "rgba(255, 255, 255, 1.0)"; // white 
+    ctx.fillRect(x, y, w+32, h+32+32); 
+    writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[255,255,255,255],msgstr); 
+    writecStr(x+288,y+h+24,432,128,"ubuntufont",[0,0,0,255],[255,255,255,255],time); 
+  } else if(minfo==3) { 
+    var time = getTime("12h");  
+    x=600; y=1408-(h+64)-32; 
+    ctx.fillStyle = "rgba(200, 240, 200, 1.0)"; // blue  
+    ctx.fillRect(x, y, w+32, h+32+32); 
+    writecStr(x+16,y+16,432,128,"ubuntubold",[0,0,0,255],[200,240,200,255],msgstr); 
+    writecStr(x+168,y+h+24,432,128,"ubuntufont",[0,0,0,255],[200,240,200,255],time); 
+  } 
+  imgData = ctx.getImageData(0,1408-(h+64)-32,1080,h+64+8); 
+  for (let i=0; i<1080*4*(h+64+8); i+=4) { 
+	  appView[appPtr*1080*4+i+0]=imgData.data[i+0]; 
+	  appView[appPtr*1080*4+i+1]=imgData.data[i+1];
+	  appView[appPtr*1080*4+i+2]=imgData.data[i+2];
+	  appView[appPtr*1080*4+i+3]=imgData.data[i+3];
+  } 
+  appPtr += (h+64+8); 
+}
+
 function send() { 
   var cnv = document.getElementById("canvas"); 
   var ctx = cnv.getContext('2d'); 
