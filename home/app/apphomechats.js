20f6a443e5cbb0d75d52459ea50fb413204f0d26
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-17 10:39:55 +0530

    Update apphomechats.js

--- home/app/apphomechats.js
+++ home/app/apphomechats.js
@@ -129,7 +129,7 @@ var ptr=appPtr;
               } else if (x>360*2 && x<360*3 && y>0 && y<144) { 
               } else if (x>1080-128 && x<1080 && y>144*1 && y<144*2) { 
 		      if (ptr>=32) {ptr-=32;} 
-		      showMessage(0,144,1080,1880,0,ptr); 
+		      showMessage(0,144,1080,1232,0,ptr); 
               } else if (x>0 && x<1080 && y>144*2 && y<144*3) { 
               } else if (x>0 && x<1080 && y>144*3+8 && y<144*3+8+128) { 
               } else if (x>0 && x<1080 && y>144*3 && y<144*3+144) { 
@@ -140,7 +140,7 @@ var ptr=appPtr;
 	      } else if (x>0 && x<1080 && y>160*3 && y<160*3+144) { 
 	      } else if (x>1080-128 && x<1080 && y>1280 && y<1408) { 
 		      if (ptr<appPtr) {ptr+=32;}  
-		      showMessage(0,144,1080,1880,0,ptr); 
+		      showMessage(0,144,1080,1232,0,ptr); 
 	      } else if (x>680 && x<680+64 && y>1440 && y<1440+64) { 
 		      /* clearInterval(timer); */ attachment(); 
 	      } else if (x>808 && x<808+64 && y>1440 && y<1440+64) { 
