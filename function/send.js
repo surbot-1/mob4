ec919e1be4421ad7c54163c64b03d897fcdc927e
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-17 12:52:14 +0530

    Update send.js

--- function/send.js
+++ function/send.js
@@ -120,7 +120,7 @@ function send() {
     writecStr(1080-16-16-12*24,y+h+24,w,h,"ubuntufont",[0,0,0,255],[200,240,200,255],time); 
     // setTimeout(() => {msgStatus(1080-16-16-4*24,y+h+24,"send");},1000); 
     setTimeout(() => {msgStatus(1080-16-16-4*24,y+h+24,"sent");},2000); 
-    setTimeout(() => {msgStatus(1080-16-16-4*24,y+h+24,"dlvd");},3000); 
+    // setTimeout(() => {msgStatus(1080-16-16-4*24,y+h+24,"dlvd");},3000); 
     setTimeout(() => {msgStatus(1080-16-16-4*24,y+h+24,"seen");},4000);  
   } else if(minfo==1) { 
     var time = getTime("12h"); 
