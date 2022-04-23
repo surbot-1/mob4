6244dedd372afda80f56330849e366dd720adbb2
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-23 06:31:11 +0530

    Update initiate.js

--- initialize/initiate.js
+++ initialize/initiate.js
@@ -62,7 +62,7 @@ var ptr=0; var ptrp=0;
 	
 	
  function updtCursor(rptr, rptrp) { 
-  var x=0; var y=0; var ptr=0; 
+  var x=0; var y=0; var ptr=0; var ci=0; var cj=0; 
   var buf = new ArrayBuffer(4); 
   var view = new DataView(buf); 
   if (rptrp==0) {x=200+16; y=144*2+8+40;} 
@@ -89,7 +89,7 @@ var ptr=0; var ptrp=0;
  }
 	
  function storekeyvalue(viewByte,rptr) { 
-  var x=0; var y=0; var ptr=0; 
+  var x=0; var y=0; var ptr=0; var ci=0; var cj=0; 
   var buf = new ArrayBuffer(4); 
   var view = new DataView(buf); 
   if (rptr==0) {x=200+16; y=144*2+8+40;} 
