e23b9c8b8c73b663cc2c89f48121514ec619e9c2
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-23 21:15:29 +0530

    Update apphome.js

--- home/app/apphome.js
+++ home/app/apphome.js
@@ -10,26 +10,56 @@ ctx.fillRect(0, 0, 1080, 144);
   fileViewerUrl(920, 2024, "icon/add-icon-128.bmp"); 
 	
 ctx.fillStyle = "rgba(240, 240, 240, 1.0)";  // lightgray 
-ctx.fillRect(0, 144, 1080, 2032);  
-fileViewerUrl(64, 144*1+8, "icon/business-woman-icon-128.bmp"); 
+ctx.fillRect(0, 144, 1080, 2032); 
+	
+	
+/* fileViewerUrl(64, 144*1+8, "icon/business-woman-icon-128.bmp"); 
 writecStr(200,144*1+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"sAy"); 
 fileViewerUrl(64, 144*2+8, "icon/business-man-icon-128.bmp"); 
 writecStr(200,144*2+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"Yash Shiv"); 
 fileViewerUrl(64, 144*3+8, "icon/business-man-icon-128.bmp"); 
-writecStr(200,144*3+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"Jitendra"); 
+writecStr(200,144*3+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"Jitendra"); */ 
+	
+var fname=[""]; 
+	
+function writefName() {  
+     for (let i=1; i<appfriendcount+1; i++) {
+	fileViewerUrl(64, 144*i+8, "icon/business-woman-icon-128.bmp"); 
+        writecStr(200,144*i+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],fname[i]); 
+     }
+}
 	
 if (true) { 
    var tmr; appfriendcount=0; 
-   var countf = readAppFriendCount(sender); 
+   var fcount = readAppFriendCount(sender); 
    tmr = setInterval( ()=> { 
    if (appfriendcount) { 
        clearInterval(tmr); 
-       for (let i=0; i<appfriendcount+1; i++) { 
+       for (let i=1; i<appfriendcount+1; i++) { 
 	   readAppFriend(sender, i); 
        } 
    } 
    }, 0200); 
-}
+} 
+	
+if (true) { 
+   var tmr; appfriendname=""; 
+   var rcvcmp = fndView[appfriendcount*32+0]; 
+   tmr = setInterval( ()=> { 
+     if (rcvcmp) { 
+       clearInterval(tmr); 
+       for (let i=1; i<appfriendcount+1; i++) { 
+	  for (let j=0; i<(fndView[i*32+2]); j++) { 
+	    fname[i] += ascChar(fndView[i*32+8+j]); 
+	  } 
+        writefName(); 
+       } 
+     } else if (!rcvcmp) { 
+       rcvcmp = fndView[appfriendcount*32+0]; 
+     }
+   }, 0200); 
+} 
+	
 	
 	var timer;
 	function check() { 
