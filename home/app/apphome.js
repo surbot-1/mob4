a9ab8bbedab6775c62c7fe07c5f721954e549acb
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-23 20:42:49 +0530

    Update apphome.js

--- home/app/apphome.js
+++ home/app/apphome.js
@@ -12,10 +12,10 @@ ctx.fillRect(0, 0, 1080, 144);
 ctx.fillStyle = "rgba(240, 240, 240, 1.0)";  // lightgray 
 ctx.fillRect(0, 144, 1080, 2032);  
 fileViewerUrl(64, 144*1+8, "icon/business-woman-icon-128.bmp"); 
-fileViewerUrl(64, 144*2+8, "icon/business-man-icon-128.bmp"); 
-fileViewerUrl(64, 144*3+8, "icon/business-man-icon-128.bmp"); 
 writecStr(200,144*1+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"sAy"); 
+fileViewerUrl(64, 144*2+8, "icon/business-man-icon-128.bmp"); 
 writecStr(200,144*2+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"Yash Shiv"); 
+fileViewerUrl(64, 144*3+8, "icon/business-man-icon-128.bmp"); 
 writecStr(200,144*3+48,480,128,"ubuntubold",[0,0,0,255],[240,240,240,255],"Jitendra"); 
 	
 if (true) { 
