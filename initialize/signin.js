113fb07bc21c75ba9f5b46c16e34a69a5b714c5f
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-23 11:34:18 +0530

    Update signin.js

--- initialize/signin.js
+++ initialize/signin.js
@@ -1,36 +1,34 @@
 function gotoSignin() { 
-  var unamed; var passd: 
-  var unamea; var passa; 
-                 getUserSigninInfo(); 
-                 getAppSigniupInfo(); 
-             setTimeout(function(){
-             if (unamed == unamea && passd == passa) {
+      var unamed; var passd: 
+      var unamea; var passa; 
+      getUserSigninInfo(); 
+      getAppSigniupInfo(); 
+      setTimeout(function(){
+         if (unamed == unamea && passd == passa) {
                  signin = true; 
                  var date = getDate("ddmmyyyy"); 
                  var time = getTime("12h"); 
-              var ref = firebase.database().ref("App/User/Signin").set({
+              var ref = firebase.database().ref("App/User/Signin"); 
+              ref.set({
                   Username: unamed, 
                   Password: passd, 
                   Date: date, 
                   Tate: time, 
                   ip: "ip"
               }); 
-                  // initiateChat();  
-                  }
-                  
-              else { 
-                  signin = false; 
-                   } 
-                  } , 5000 );
+           } else { 
+              signin = false; 
+           } 
+        } , 5000 );
                 
-              function getUserSigninInfo() {
+        function getUserSigninInfo() {
                   
-                  } 
-               function getAppSignupInfo() {
-                 var ref = firebase.database().ref("App/User/Signup");
-                 ref.once("value", function(snapshot) { 
-                 unamea = snapshot.child("username").val();
-                 passa = snapshot.child("password").val();
-                  });
-                 }  
+        } 
+        function getAppSignupInfo() {
+            var ref = firebase.database().ref("App/User/Signup");
+            ref.once("value", function(snapshot) { 
+            unamea = snapshot.child("username").val();
+            passa = snapshot.child("password").val();
+            });
+        }  
   }
