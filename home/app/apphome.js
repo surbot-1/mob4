a58c7062853dc448d1868dcb29e5c910b33bdb8b
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-23 08:04:01 +0530

    Update apphome.js

--- home/app/apphome.js
+++ home/app/apphome.js
@@ -33,15 +33,20 @@ fileViewerUrl(920, 1896, "icon/add-icon-128.bmp");
 		      clearInterval(timer); appHomeChatsChatbot(); 
               } else if (x>0 && x<1080 && y>144*2 && y<144*3) { 
 		      clearInterval(timer); 
-		      sender = "Jitendra"; receiver = "Yash Shiv"; 
+		      receiver = "Yash Shiv"; 
 		      appHomeChatsUser(receiver); 
               } else if (x>0 && x<1080 && y>144*3 && y<144*4) { 
 		      clearInterval(timer); 
-		      sender = "Yash Shiv"; receiver = "Jitendra"; 
+		      receiver = "Jitendra"; 
 		      appHomeChatsUser(receiver); 
               } else if (x>0 && x<1080 && y>144*4 && y<144*5) { 
-		      // clearInterval(timer); appHomeChatsUser(""); 
-              } else if (x>0 && x<1080 && y>160*5 && y<160*5+144) { 
+		      clearInterval(timer); 
+		      receiver = "sAy1"; 
+		      appHomeChatsUser(receiver); 
+              } else if (x>0 && x<1080 && y>144*5 && y<144*5+144) { 
+		      clearInterval(timer); 
+		      receiver = "sAy2"; 
+		      appHomeChatsUser(receiver); 
 	      } else if (x>0 && x<1080 && y>160*6 && y<160*6+144) { 
 	      } else if (x>0 && x<1080 && y>160*7 && y<160*7+144) { 
 	      } else if (x>0 && x<1080 && y>160*8 && y<160*8+144) { 
