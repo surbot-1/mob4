7c72d1c0a3f5128d06c3df67310639114ed5fc58
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-25 18:47:53 +0530

    Update signup.js

--- initialize/signup.js
+++ initialize/signup.js
@@ -15,10 +15,6 @@ ctx.fillStyle = "rgba(240, 240, 240, 1.0)"; // light gray
 ctx.fillRect(0, 144, 1080, 2032); 
 
 ctx.fillStyle = "rgba(0,0,128,1.0)"; // blue 
-ctx.fillRect(200, 144*1+8, 680, 128); 
-ctx.fillStyle = "rgba(255,255,255,1.0)"; // white
-ctx.fillRect(200+8, 144*1+8+8, 680-16, 128-16); 
-ctx.fillStyle = "rgba(0,0,128,1.0)"; // blue 
 ctx.fillRect(200, 144*2+8, 680, 128); 
 ctx.fillStyle = "rgba(255,255,255,1.0)"; // white
 ctx.fillRect(200+8, 144*2+8+8, 680-16, 128-16); 
@@ -32,9 +28,13 @@ ctx.fillStyle = "rgba(255,255,255,1.0)"; // white
 ctx.fillRect(200+8, 144*4+8+8, 680-16, 128-16); 
 ctx.fillStyle = "rgba(0,0,128,1.0)"; // blue 
 ctx.fillRect(200, 144*5+8, 680, 128); 
-  writecStr(200+268,144*5+8+40,24*6,48,"font2448",[255,255,255,255],[0,0,128,255],"SignUp"); 
-  // writecStr(200,144*6+8+48,24*9,32,"ubuntufont",[32,32,32,255],[240,240,240,255],"Password?"); 
-  writecStr(200+24*22,144*6+8+48,24*6,32,"ubuntufont",[32,32,32,255],[240,240,240,255],"SignIn"); 
+ctx.fillStyle = "rgba(255,255,255,1.0)"; // white
+ctx.fillRect(200+8, 144*5+8+8, 680-16, 128-16); 
+ctx.fillStyle = "rgba(0,0,128,1.0)"; // blue 
+ctx.fillRect(200, 144*6+8, 680, 128); 
+  writecStr(200+268,144*6+8+40,24*6,48,"font2448",[255,255,255,255],[0,0,128,255],"SignUp"); 
+  // writecStr(200,144*7+8+48,24*9,32,"ubuntufont",[32,32,32,255],[240,240,240,255],"Password?"); 
+  writecStr(200+24*22,144*7+8+48,24*6,32,"ubuntufont",[32,32,32,255],[240,240,240,255],"SignIn"); 
 
 for (let i=0; i<10; i++) {
      signByte[i*32+0]=0; 
@@ -49,7 +49,7 @@ for (let i=0; i<10; i++) {
 
 ktype=0; 
 drawKeypad(0,1664,ktype); 
-showCursor(200+16,144*1+8+40); 
+showCursor(200+16,144*2+8+40); 
 var ptr=2; var ptrp=2; 
   
   
