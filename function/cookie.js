c8293c3e9387b3c9204bffac12e0765aa8502b06
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-26 20:02:59 +0530

    Update and rename cookies.js to cookie.js

--- function/cookie.js
+++ function/cookie.js
@@ -1,7 +1,7 @@
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
+  let expires = "expires="+d.toUTCString(); alert((d.toUTCString())); 
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
