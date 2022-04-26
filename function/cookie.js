30984c2d9b149ce4fad6164f915ada4168f5d9b6
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-26 20:07:56 +0530

    Update cookie.js

--- function/cookie.js
+++ function/cookie.js
@@ -1,6 +1,6 @@
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  const d = new Date(); alert(d); 
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000)); alert(); 
   let expires = "expires="+d.toUTCString(); alert((d.toUTCString())); 
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
