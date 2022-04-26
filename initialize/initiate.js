48f2e9a4d5b506aa01be6c1d876b850f6678f01b
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-27 01:20:33 +0530

    Create initiate.js

---
+++ initialize/initiate.js
@@ -0,0 +1,23 @@
+function initiate() { 
+  var uname = getCookie("username"); 
+  var  pass = getCookie("password"); 
+  if (uname="" && pass="") { 
+    signin(); 
+  } else if (uname!="" && pass!="") { 
+    if (uname=="sAy1" && pass=="s@1234") { 
+      username=uname; password=pass; 
+      sender=uname; appHomeChats(); 
+    } else if (uname=="sAy2" && pass=="k@1234") { 
+      username=uname; password=pass; 
+      sender=uname; appHomeChats(); 
+    } else if (uname=="Yash Shiv" && pass=="ys@1234") { 
+      username=uname; password=pass; 
+      sender=uname; appHomeChats(); 
+    } else if (uname=="Jitendra" && pass=="j@1234") { 
+      username=uname; password=pass; 
+      sender=uname; appHomeChats(); 
+    } else { 
+      signin(); 
+    }
+  } 
+}
