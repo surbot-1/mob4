450c74812e2c2eea77b7097c3fc1eb0e826221c8
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-27 20:20:50 +0530

    Update notification.js

--- function/notification.js
+++ function/notification.js
@@ -1,6 +1,51 @@
 function notification() { 
   
-  askNotificationPermission(); 
+  (async () => {
+    // create and show the notification
+    const showNotification = () => {
+        // create a new notification
+        const notification = new Notification('JavaScript Notification API', {
+            body: 'This is a JavaScript Notification API demo',
+            icon: './elephant.bmp'
+        });
+
+        // close the notification after 10 seconds
+        setTimeout(() => {
+            notification.close();
+        }, 10 * 1000);
+
+        // navigate to a URL when clicked
+        notification.addEventListener('click', () => {
+
+            window.open('https://www.javascripttutorial.net/web-apis/javascript-notification/', '_blank');
+        });
+    }
+
+    // show an error message
+    const showError = () => {
+        const error = document.querySelector('.error');
+        error.style.display = 'block';
+        error.textContent = 'You blocked the notifications';
+    }
+
+    // check notification permission
+    let granted = false;
+
+    if (Notification.permission === 'granted') {
+        granted = true;
+    } else if (Notification.permission !== 'denied') {
+        let permission = await Notification.requestPermission();
+        granted = permission === 'granted' ? true : false;
+    }
+
+    // show notification or error
+    granted ? showNotification() : showError();
+
+})();
+  
+}
+  
+  /* askNotificationPermission(); 
   function askNotificationPermission() {
   // function to actually ask the permissions
   function handlePermission(permission) {
@@ -27,6 +72,6 @@ function notification() {
       });
     }
   }
-}
+} */ 
 
-}
+//}
