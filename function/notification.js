12689d45c7fed88a83e34a450935d721991ecd8f
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-27 20:50:35 +0530

    Update notification.js

--- function/notification.js
+++ function/notification.js
@@ -1,6 +1,34 @@
 function notification() { 
+  notifyMe(); 
+  function notifyMe() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
+}
+
+}
   
-  (async () => {
+  /* (async () => {
     // create and show the notification
     var showNotification = () => {
         // create a new notification
@@ -43,9 +71,9 @@ function notification() {
     // show notification or error
     granted ? showNotification() : showError();
 
-})();
+})(); */ 
   
-}
+// }
   
   /* askNotificationPermission(); 
   function askNotificationPermission() {
