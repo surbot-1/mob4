05c9769eca56da5ec68f91fa1d3b4d97b6955485
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-27 16:32:56 +0530

    Create receive.js

---
+++ function/receive.js
@@ -0,0 +1,48 @@
+function receive(t) { 
+  var timer; 
+  function rcvMsg() { 
+    if (useractive) { 
+      rcvmsgid=rcvmsgidp+1; rcvmsgsts="Null"; 
+      var mid = readAppMessageStatus(receiver,sender,rcvmsgid); 
+      if (rcvmsgsts=="Null") { 
+        var tmr = setInterval( ()=> { 
+          if (rcvmsgsts!="Null") { 
+            clearInterval(tmr); 
+            if (rcvmsgsts=="sent") { 
+              rcvmsgidp=rcvmsgid; rcvmsgid=rcvmsgidp+1;
+              rcvmsgstsp=rcvmsgsts; rcvmsgsts="Null"; 
+              readAppMessage(receiver,sender,rcvmsgidp); 
+              writeAppMessageStatus(receiver,sender,rcvmsgidp,"seen"); 
+              /* } else if (rcvmsgsts=="dlvd") {  
+              rcvmsgidp=rcvmsgid; rcvmsgid=rcvmsgidp+1;
+              rcvmsgstsp=rcvmsgsts; rcvmsgsts="Null"; 
+              // readAppMessage(receiver,sender,rcvmsgidp); 
+              // writeAppMessageStatus(receiver,sender,rcvmsgidp,"dlvd"); */
+            } else if (rcvmsgsts=="seen") { 
+              rcvmsgidp=rcvmsgid; rcvmsgid=rcvmsgidp+1;
+              rcvmsgstsp=rcvmsgsts; rcvmsgsts="Null"; 
+            } 
+          } 
+        }, 0020); 
+        setTimeout( ()=> {clearInterval(tmr);}, 1000); 
+      } else if (rcvmsgsts!="Null") { 
+        if (rcvmsgsts=="sent") { 
+          rcvmsgidp=rcvmsgid; rcvmsgid=rcvmsgidp+1; 
+          rcvmsgstsp=rcvmsgsts; rcvmsgsts="Null"; 
+          readAppMessage(receiver,sender,rcvmsgid); 
+          writeAppMessageStatus(receiver,sender,rcvmsgidp,"seen"); 
+          /* } else if (rcvmsgsts=="dlvd") { 
+          rcvmsgidp=rcvmsgid; rcvmsgid=rcvmsgidp+1;
+          rcvmsgstsp=rcvmsgsts; rcvmsgsts="Null"; 
+          // readAppMessage(receiver,sender,rcvmsgidp); 
+          // writeAppMessageStatus(receiver,sender,rcvmsgidp,"dlvd"); */
+        } else if (rcvmsgsts=="seen") { 
+          rcvmsgidp=rcvmsgid; rcvmsgid=rcvmsgidp+1;
+          rcvmsgstsp=rcvmsgsts; rcvmsgsts="Null"; 
+        } 
+      } 
+    } 
+  } 
+  timer = setInterval(rcvMsg, 2000); 
+
+}
