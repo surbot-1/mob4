6858c8561d491c062cf2f7170a19264acf25d0ef
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-04-28 11:58:04 +0530

    Update worker.js

--- function/worker.js
+++ function/worker.js
@@ -1,6 +1,6 @@
 onmessage = function(e) {
   // console.log('Worker: Message received from main script');
-  const result = e.data[0] * e.data[1];
+  const result = "e.data[0] * e.data[1]";
   if (isNaN(result)) {
     postMessage('Please write two numbers');
   } else {
