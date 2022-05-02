2d9321b511b8ce0e6adc1cec2de330b8694416c8
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-05-02 07:17:21 +0530

    Update music.js

--- function/music.js
+++ function/music.js
@@ -1,5 +1,7 @@
-var audio = new Audio('audio/path.mp3');
+var msgsendtone = new Audio('music/message-send.mp3'); 
+var msgseentone = new Audio('music/message-tone.mp3'); 
+var msgnotitone = new Audio('music/pani-ki-boond.mp3');
 
-function playSound(){
-    audio.play();
+function playSound(a){
+    a.play();
 }
