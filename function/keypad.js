0c6013fdce38af15ce14f5c1371cc5d091adb034
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-05-03 12:33:06 +0530

    Update keypad.js

--- function/keypad.js
+++ function/keypad.js
@@ -147,10 +147,12 @@ function readKeypad(x, y, t) {
 	   ctx.putImageData(imgData, kx+kl, ky+kt); 
 	   
 	   var font = "font2448";  
+           var fcolor=[0,0,0,255]; 
+           var bcolor=[208,208,208,255]; 
 	   var str = keyChar(kc, kr, t); 
 	   var ki = kx+kl+(kw+pw-(str.length)*24)/2;  
 	   var kj = ky+kt+(kh+ph-48)/2; 
-	   writeStr(ki, kj, 24*5, 48*1, font, str); 
+	   writecStr(ki, kj, 24*5, 48*1, font, fcolor, bcolor, str); 
         }
 	function f2() {
 	   var kx=x+px+(kw+kl*2)*kc; 
