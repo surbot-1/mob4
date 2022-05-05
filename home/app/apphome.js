087d082fd2cc5a92a1ae67b904f5572a116957c5
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-05-05 21:29:03 +0530

    Update apphome.js

--- home/app/apphome.js
+++ home/app/apphome.js
@@ -178,7 +178,7 @@ msgid=1; rcvmsgid=0; rcvmsgidp=0;
 		       }
               } else if (x>0 && x<1080 && y>144*2 && y<144*11) { 
 		 for (let i=2; i<appcontcount+1; i++) { 
-                    if (x>0 && x<1080 && y>144*i && y<144*(contView[0]+1)) {
+                    if (x>0 && x<1080 && y>144*i && y<144*(appcontcount+1)) {
 		      if (acsts[i]) { 
 			   clearInterval(timer); 
 			   /* var acname=""; var acuname=""; 
