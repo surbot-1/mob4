58f1f4b4fb0ee737a9c843b8eb91494954f63f82
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-05-08 10:04:34 +0530

    Update indexeddb.js

--- function/indexeddb.js
+++ function/indexeddb.js
@@ -1,1 +1,13 @@
-
+         //prefixes of implementation that we want to test
+         window.indexedDB = window.indexedDB || window.mozIndexedDB || 
+         window.webkitIndexedDB || window.msIndexedDB;
+         
+         //prefixes of window.IDB objects
+         window.IDBTransaction = window.IDBTransaction || 
+         window.webkitIDBTransaction || window.msIDBTransaction;
+         window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+         window.msIDBKeyRange; 
+         
+         if (!window.indexedDB) {
+            window.alert("Your browser doesn't support a stable version of IndexedDB."); 
+         } else { }
